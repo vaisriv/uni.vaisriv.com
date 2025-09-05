83c361ee5873046c8fdf835dd11d61854b946d2a
vai <vai.sriv@icloud.com>
2025-09-04 23:40:19 -0400

    feat(quartz): add latex macros for engineering/math symbols and functions

--- quartz.config.ts
+++ quartz.config.ts
@@ -77,7 +77,70 @@ const config: QuartzConfig = {
         externalLinkIcon: true,
       }),
       Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Latex({ renderEngine: "katex", customMacros: {
+        // symbols
+        "\\questeq": "\\stackrel{?}{=}",
+        "\\st": "\\text{ s.t. }",
+        "\\wrt": "\\text{ wrt. }",
+
+        // vectors
+        "\\crvector": "\\begin{bmatrix}#1\\end{bmatrix}",
+        "\\vecb": "\\vec{\\bm{#1}}",
+
+        // functions
+        "\\func": "\\mathrm{#1}",
+        "\\fn": "\\mathrm{#1}(#2)",
+
+        // derivatives
+        "\\drv": "\\frac{\\mathrm{d}#1}{\\mathrm{d}#2}",
+        "\\ddrv": "\\frac{\\mathrm{d}^{2}#1}{\\mathrm{d}{#2}^{2}}",
+        "\\deriv": "\\frac{\\mathrm{d}}{\\mathrm{d}#1}\\left(#2\\right)",
+        "\\dderiv": "\\frac{\\mathrm{d}^{2}}{\\mathrm{d}{#1}^{2}}\\left(#2\\right)",
+
+        // partial derivatives
+        "\\pdrv": "\\frac{\\partial#1}{\\partial#2}",
+        "\\pddrv": "\\frac{\\partial^{2}#1}{\\partial{#2}^{2}}",
+        "\\pderiv": "\\frac{\\partial}{\\partial#1}\\left(#2\\right)",
+        "\\pdderiv": "\\frac{\\partial^{2}}{\\partial{}{#1}^{2}}\\left(#2\\right)",
+
+        // probability
+        "\\E": "\\mathrm{E}(#1)",
+        "\\Var": "\\mathrm{Var}(#1)",
+        "\\Cov": "\\mathrm{Cov}(#1)",
+        "\\Bias": "\\mathrm{Bias}(#1)",
+        "\\Prob": "\\mathrm{Pr}(#1)",
+
+        // trig
+        "\\sin": "\\mathrm{sin}(#1)",
+        "\\cos": "\\mathrm{cos}(#1)",
+        "\\tan": "\\mathrm{tan}(#1)",
+        "\\csc": "\\mathrm{csc}(#1)",
+        "\\sec": "\\mathrm{sec}(#1)",
+        "\\cot": "\\mathrm{cot}(#1)",
+        "\\sinh": "\\mathrm{sinh}(#1)",
+        "\\cosh": "\\mathrm{cosh}(#1)",
+        "\\tanh": "\\mathrm{tanh}(#1)",
+        "\\coth": "\\mathrm{coth}(#1)",
+        "\\arcsin": "\\mathrm{arcsin}(#1)",
+        "\\arccos": "\\mathrm{arccos}(#1)",
+        "\\arctan": "\\mathrm{arctan}(#1)",
+
+        // log, ln, etc
+        "\\log": "\\mathrm{log}(#1)",
+        "\\ln": "\\mathrm{ln}(#1)",
+
+        // num sets
+        "\\Re": "\\mathrm{Re}\\{#1\\}",
+        "\\Im": "\\mathrm{Im}\\{#1\\}",
+
+        // laplace
+        "\\laplace": "\\mathscr{L}\\{#1\\}",
+        "\\ilaplace": "\\mathscr{L}^{-1}\\{#1\\}",
+
+        // integrals
+        "\\inti": "\\int #1 \\mathrm{d}#2",
+        "\\intd": "\\int_{#1}^{#2} #3 \\mathrm{d}#4",
+      }}),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
